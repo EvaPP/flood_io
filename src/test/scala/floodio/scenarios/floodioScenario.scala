89fd058893599b802b2be90d1a8e604b2d1c4885
Yevheniia_Pasiechna <Yevheniia_Pasiechna@epam.com>
2017-05-10 10:34:35 +0300

    Code refactoring

--- src/test/scala/floodio/scenarios/floodioScenario.scala
+++ src/test/scala/floodio/scenarios/floodioScenario.scala
@@ -5,8 +5,6 @@ import io.gatling.core.Predef.scenario
 import io.gatling.core.Predef._
 import floodio.constants
 
-import scala.util.Random
-
 object floodioScenario {
 
   val min_think_time = constants.config.getInt("min_think_time")
@@ -14,19 +12,19 @@ object floodioScenario {
 
   val scn = scenario("RecordedSimulation")
     .exec(floodioRequests.start)
-    .pause(Random.nextInt(max_think_time - min_think_time) + min_think_time)
+    .pause(min_think_time, max_think_time)
     .exec(floodioRequests.step1)
-    .pause(Random.nextInt(max_think_time - min_think_time) + min_think_time)
+    .pause(min_think_time, max_think_time)
     .exec(floodioRequests.step2)
     .foreach("${order_list}", "order") {
       floodioRequests.extractSelectedOrder
     }
-    .pause(Random.nextInt(max_think_time - min_think_time) + min_think_time)
+    .pause(min_think_time, max_think_time)
     .exec(floodioRequests.step3)
-    .pause(Random.nextInt(max_think_time - min_think_time) + min_think_time)
+    .pause(min_think_time, max_think_time)
     .exec(floodioRequests.createChalangerOrderMap)
     .exec(floodioRequests.step4)
-    .pause(Random.nextInt(max_think_time - min_think_time) + min_think_time)
+    .pause(min_think_time, max_think_time)
     .exec(floodioRequests.step5)
 
 }
